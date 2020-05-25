8c7735f7f7c340f39e93df7d4e8a7d70b3549f0b
Sharat srivatsa N <62974846+sharat999333@users.noreply.github.com>
2020-05-25 17:12:18 +0530

    Updates

---
+++ precache-manifest.772323044598f30a6b78946dc689b6af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb867378f49f397df67fb114adc42374",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "a9c527cffd058e6a7d9f",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c623d30628c24fefe589",
+    "url": "/react-deploy/static/js/2.ca0397b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.ca0397b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9c527cffd058e6a7d9f",
+    "url": "/react-deploy/static/js/main.1236b96b.chunk.js"
+  },
+  {
+    "revision": "88cc752382718b1d2923",
+    "url": "/react-deploy/static/js/runtime-main.625baed9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
